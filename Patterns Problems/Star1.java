42ae59fa9bae02aa6d5b18a8550db7de36710c2e
KALYANI SANJAY MHALA <84217977+kalyani2003@users.noreply.github.com>
2023-07-19 12:36:52 +0530

    Add files via upload

---
+++ Patterns Problems/Star1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Star1{
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 5; i++){
+
+            for (int j =1; j <= i; j++){
+            System.out.print("*" );  /// dont use *** println ***
+            }
+            System.out.println();  /// for printing next line star....
+        }
+
+    }
+}
